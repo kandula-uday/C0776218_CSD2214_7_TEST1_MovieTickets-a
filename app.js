703420e2b04c2c3c3ea2380df06ea9c217dd2da3
uday shankar kandula <65588605+kandula-uday@users.noreply.github.com>
2021-03-12 06:31:13 +0530

    js modification

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const movieSelect = document.getElementById('movies');
 
-document.getElementById('').onclick = function() {
+
+document.getElementById('buy-seats').onclick = function() {
   var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  alert(checkboxes.length);
-}
+   alert(checkboxes.length);
+   alert(movieSelect);
+
+ }
+
+
+// function getCheckboxCount() {
+//     return document.querySelectorAll('input[type="checkbox"]:checked').length;
+//   }
+
