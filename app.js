833532225709fdb76e8ea9fa5e11b59b591632a8
uday shankar kandula <65588605+kandula-uday@users.noreply.github.com>
2021-03-12 06:09:48 +0530

    changing id

---
+++ app.js
@@ -0,0 +1,6 @@
+const movieSelect = document.getElementById('movies');
+
+document.getElementById('').onclick = function() {
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  alert(checkboxes.length);
+}
